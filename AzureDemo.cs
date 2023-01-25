cb1078f2c9d7588c82a9fbda6de183b04ab88d2e
Gordon Lee <gordonlee97@gmail.com>
2023-01-25 10:41:31 -0800

    Add files via upload

---
+++ AzureDemo.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileUploadPrototype
+{
+    internal class AzureDemo
+    {
+        public void AzureFileUpload(string fileName, string containerName)
+        {
+            string myFile,
+                myFileName,
+                myConnectionString;
+            Stream file;
+
+            myConnectionString = "DefaultEndpointsProtocol=https;AccountName=gordonhomerg8039;AccountKey=L4a620Ik3qt9u3pgRO4avbz9kfr1XD5FGGbHMDYmbJKTlib7THYNSAZBGKZuKgs+bix+0GUjLSab+AStGMIVRA==;EndpointSuffix=core.windows.net";
+
+
+            file = new FileStream(fileName, FileMode.Open);
+
+            CloudStorageAccount myCloudStorageAccount = CloudStorageAccount.Parse(myConnectionString);
+            CloudBlobClient blobClient = myCloudStorageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+
+            if (container.CreateIfNotExists())
+            {
+                container.SetPermissionsAsync(new BlobContainerPermissions
+                {
+                    PublicAccess = BlobContainerPublicAccessType.Blob
+                });
+            }
+
+            myFile = Path.GetExtension(fileName);
+            myFileName = Path.GetExtension(fileName);
+
+            CloudBlockBlob myBlockBlob = container.GetBlockBlobReference(myFileName);
+            myBlockBlob.Properties.ContentType = myFile;
+
+            //upload file to blob storage
+            myBlockBlob.UploadFromStreamAsync(file);
+
+            Console.WriteLine("Upload Successful");
+        }
+
+
+        public void AzureFileDownload(string fileName, string containerName)
+        {
+            string myConnectionString = "DefaultEndpointsProtocol=https;AccountName=gordonhomerg8039;AccountKey=L4a620Ik3qt9u3pgRO4avbz9kfr1XD5FGGbHMDYmbJKTlib7THYNSAZBGKZuKgs+bix+0GUjLSab+AStGMIVRA==;EndpointSuffix=core.windows.net";
+
+            CloudStorageAccount myCloudStorageAccount = CloudStorageAccount.Parse(myConnectionString);
+            CloudBlobClient myBlob = myCloudStorageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer myContainer = myBlob.GetContainerReference(containerName);
+            CloudBlockBlob myBlockBlob = myContainer.GetBlockBlobReference(fileName);
+
+            Stream fileupd = File.OpenWrite(@"C:\Users\gordo\Downloads\" + fileName);
+            myBlockBlob.DownloadToStream(fileupd);
+
+            Console.WriteLine("Download successful?");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace FileUploadPrototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AzureDemo azureDemo = new AzureDemo();
+
+            azureDemo.AzureFileUpload(@"..\..\Files\azurenote.txt", "democontainer");
+            //azureDemo.AzureFileDownload(".txt", "democontainer");
+
+            Console.ReadKey(); 
+        }
+    }
+}
